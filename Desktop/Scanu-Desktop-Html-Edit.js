f18bf7e883d02d7a3c132832d15e35d8f8f79d05
ScanuNicco <Scanuproductions@gmail.com>
2018-03-10 17:22:37 -0800

    Rag OS 1.4
    >Fixed download links in HTML Editor and Notepad
    >Added support for Safari

--- Desktop/Scanu-Desktop-Html-Edit.js
+++ Desktop/Scanu-Desktop-Html-Edit.js
@@ -34,3 +34,52 @@ function runCode() {
     var code = document.getElementById("htmlInput").value;
     document.getElementById("htmlDisplay").innerHTML == "code";
 }
+
+    
+function runPreview() {
+    var input = document.getElementById("input").value;
+    document.getElementById("preview").innerHTML = input;
+}
+    
+function downloadHtml() {
+    var hiddenElement = document.createElement('a');
+    var downloadInput = document.getElementById('preview').innerHTML;
+    
+    hiddenElement.href = 'data:attachment/text,' + encodeURI(downloadInput);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'myHtml.html';
+    hiddenElement.click();
+}
+
+function uploadFile() {
+    var fileContent = document.getElementById("file").value;
+    document.getElementById('input').innerHTML = fileContent;
+    alert(fileContent);
+}
+    
+document.getElementById('input-file')
+  .addEventListener('change', getFile)
+
+function getFile(event) {
+	const input = event.target
+  if ('files' in input && input.files.length > 0) {
+	  placeFileContent(
+      document.getElementById('input'),
+      input.files[0])
+  }
+}
+
+function placeFileContent(target, file) {
+	readFileContent(file).then(content => {
+  	target.value = content
+  }).catch(error => console.log(error))
+}
+
+function readFileContent(file) {
+	const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsText(file)
+  })
+}
--- Desktop/Scanu-Desktop-Notepad.js
+++ Desktop/Scanu-Desktop-Notepad.js
@@ -29,3 +29,34 @@ var dm = document.getElementById('notepadApp');
 dm.addEventListener('dragstart',drag_start,false); 
 document.body.addEventListener('dragover',drag_over,false); 
 document.body.addEventListener('drop',drop,false); 
+
+//<![CDATA[
+window.onload=function(){
+(function () {
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    return textFile;
+  };
+
+
+  var create = document.getElementById('create'),
+    textbox = document.getElementById('notepadContent');
+
+  create.addEventListener('click', function () {
+    var link = document.getElementById('downloadlink');
+    link.href = makeTextFile(textbox.value);
+    link.style.display = 'block';
+  }, false);
+})();
+
+}//]]> 
