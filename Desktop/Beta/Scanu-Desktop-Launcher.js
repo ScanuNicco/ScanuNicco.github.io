eaf14b25ce87c95f0ecfe3dd965362d2f37b18b5
scanunicco <Scanuproductions@gmail.com>
2018-05-07 19:57:45 -0700

    added channel selector

--- Desktop/Beta/Scanu-Desktop-Launcher.js
+++ Desktop/Beta/Scanu-Desktop-Launcher.js
@@ -15,6 +15,14 @@ function closeLogOut() {
     document.getElementById("logOutMenu").style.display = 'none';
 }
 
+function openChannel() {
+    document.getElementById("channelMenu").style.display = 'block';
+}
+
+function closeChannel() {
+    document.getElementById("channelMenu").style.display = 'none';
+}
+
 function shutDown() {
     console.log("shutting down");
     document.getElementById("splashScreen").style.display = "block";
@@ -32,6 +40,12 @@ document.getElementById('desktopContent').onclick = function(e) {
   if(e.target != document.getElementById('logOutMenu') && e.target != document.getElementById('logOutButton')) {
       closeLogOut();
   } else {
-      console.log('You clicked inside');
+      console.log('You clicked inside the logout menu');
+  }
+  if(e.target != document.getElementById('channelMenu') && e.target != document.getElementById('ChannelButton')) {
+      closeChannel();
+      console.log('You clicked outside the channel menu');
+  } else {
+      console.log('You clicked inside the channel menu');
   }
 }
