a4a9c70794ae9b1d297d96d8243b6b00a98c79dd
scanunicco <Scanuproductions@gmail.com>
2018-09-30 15:38:50 -0700

    added more movement

--- Craftblox/PointerLockControls.js
+++ Craftblox/PointerLockControls.js
@@ -1,6 +1,14 @@
-THREE.PointerLockControls = function ( camera ) {
+/**
+ * @author mrdoob / http://mrdoob.com/
+ * @author Mugen87 / https://github.com/Mugen87
+ */
+
+THREE.PointerLockControls = function ( camera, domElement ) {
 
 	var scope = this;
+
+	this.domElement = domElement || document.body;
+	this.isLocked = false;
 
 	camera.rotation.set( 0, 0, 0 );
 
@@ -12,45 +20,66 @@ THREE.PointerLockControls = function ( camera ) {
 	yawObject.add( pitchObject );
 
 	var PI_2 = Math.PI / 2;
-	
-	// Using a vector http://threejs.org/docs/api/math/Vector2.html
-	// for virtual rotation. Basically we directly modify this vector
-	// instead of applying the movement to the rotation
-	var virtualRotation = THREE.Vector2(pitchObject.rotation.x, yawObject.rotation.y);
-	// Just a temporary vector
-	var tmp = THREE.Vector2(0, 0);
 
-	var onMouseMove = function ( event ) {
+	function onMouseMove( event ) {
 
-		if ( scope.enabled === false ) return;
+		if ( scope.isLocked === false ) return;
 
 		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
 		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-		
-		// Modify the virtual rotation instead of the real one
-		virtualRotation.y -= movementX * 0.002;
-		virtualRotation.x -= movementY * 0.002;
-		virtualRotation.x = Math.max( - PI_2, Math.min( PI_2, virtualRotation.x ) );
-		
-		// Set the movement direction
-		tmp.set(pitchObject.rotation.x - virtualRotation.x, yawObject.rotation.y - virtualRotation.y);
-		// Let's set its length to 1
-		tmp.normalize();
 
-    		// Now apply the virtual rotation
-		yawObject.rotation.y += tmp.y * 0.002;
-	  	pitchObject.rotation.x += tmp.x * 0.002;
+		yawObject.rotation.y -= movementX * 0.002;
+		pitchObject.rotation.x -= movementY * 0.002;
+
+		pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
+
+	}
+
+	function onPointerlockChange() {
+
+		if ( document.pointerLockElement === scope.domElement ) {
+
+			scope.dispatchEvent( { type: 'lock' } );
+
+			scope.isLocked = true;
+
+		} else {
+
+			scope.dispatchEvent( { type: 'unlock' } );
+
+			scope.isLocked = false;
+
+		}
+
+	}
+
+	function onPointerlockError() {
+
+		console.error( 'THREE.PointerLockControls: Unable to use Pointer Lock API' );
+
+	}
+
+	this.connect = function () {
+
+		document.addEventListener( 'mousemove', onMouseMove, false );
+		document.addEventListener( 'pointerlockchange', onPointerlockChange, false );
+		document.addEventListener( 'pointerlockerror', onPointerlockError, false );
+
 	};
 
-	this.dispose = function() {
+	this.disconnect = function () {
 
 		document.removeEventListener( 'mousemove', onMouseMove, false );
+		document.removeEventListener( 'pointerlockchange', onPointerlockChange, false );
+		document.removeEventListener( 'pointerlockerror', onPointerlockError, false );
 
 	};
 
-	document.addEventListener( 'mousemove', onMouseMove, false );
+	this.dispose = function () {
 
-	this.enabled = false;
+		this.disconnect();
+
+	};
 
 	this.getObject = function () {
 
@@ -58,14 +87,14 @@ THREE.PointerLockControls = function ( camera ) {
 
 	};
 
-	this.getDirection = function() {
+	this.getDirection = function () {
 
 		// assumes the camera itself is not rotated
 
 		var direction = new THREE.Vector3( 0, 0, - 1 );
-		var rotation = new THREE.Euler( 0, 0, 0, "YXZ" );
+		var rotation = new THREE.Euler( 0, 0, 0, 'YXZ' );
 
-		return function( v ) {
+		return function ( v ) {
 
 			rotation.set( pitchObject.rotation.x, yawObject.rotation.y, 0 );
 
@@ -77,4 +106,21 @@ THREE.PointerLockControls = function ( camera ) {
 
 	}();
 
+	this.lock = function () {
+
+		this.domElement.requestPointerLock();
+
+	};
+
+	this.unlock = function () {
+
+		document.exitPointerLock();
+
+	};
+
+	this.connect();
+
 };
+
+THREE.PointerLockControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+THREE.PointerLockControls.prototype.constructor = THREE.PointerLockControls;
